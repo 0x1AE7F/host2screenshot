b52ef90a1efec1cc67e1fd3825e0deef31066482
Julian <55046135+0x1AE7F@users.noreply.github.com>
2023-01-30 22:46:23 +0100

    Add files via upload

---
+++ go.mod
@@ -0,0 +1,19 @@
+module host2screen
+
+go 1.19
+
+require github.com/chromedp/cdproto v0.0.0-20230120182703-ecee3ffd2a24
+
+require (
+	github.com/gobwas/httphead v0.1.0 // indirect
+	github.com/gobwas/pool v0.2.1 // indirect
+	github.com/gobwas/ws v1.1.0 // indirect
+	golang.org/x/sys v0.0.0-20220928140112-f11e5e49a4ec // indirect
+)
+
+require (
+	github.com/chromedp/chromedp v0.8.7
+	github.com/chromedp/sysutil v1.0.0 // indirect
+	github.com/josharian/intern v1.0.0 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
+)
---
+++ main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"context"
+	"io/ioutil"
+	"log"
+	"os"
+
+	"github.com/chromedp/cdproto/page"
+	"github.com/chromedp/chromedp"
+)
+
+func main() {
+	argsWithoutProg := os.Args[1:]
+	if len(argsWithoutProg) <= 0 {
+		log.Println("Missing argument IP!")
+		os.Exit(1)
+	}
+	if argsWithoutProg[0] == "" {
+		log.Println("Argument IP cant be empty!")
+		os.Exit(1)
+	}
+
+	// Start Chrome
+	// Remove the 2nd param if you don't need debug information logged
+	opts := append(chromedp.DefaultExecAllocatorOptions[:],
+		chromedp.Flag("ignore-certificate-errors", "1"))
+	allocCtx, cancel := chromedp.NewExecAllocator(context.Background(), opts...)
+	defer cancel()
+	ctx, cancel := chromedp.NewContext(allocCtx, chromedp.WithLogf(log.Printf))
+	defer cancel()
+
+	log.Printf("Current IP: %s", argsWithoutProg[0])
+
+	var imageBuf []byte
+	if err := chromedp.Run(ctx, ScreenshotTasks("http://"+argsWithoutProg[0]+"/", &imageBuf)); err != nil {
+		log.Printf("Error occured while trying to capture screenshot: %s", err)
+		os.Exit(1)
+	}
+	// Write our image to file
+	if err := ioutil.WriteFile(argsWithoutProg[0]+".png", imageBuf, 0644); err != nil {
+		log.Printf("Error occured while trying to write screenshot to disk: %s", err)
+		os.Exit(1)
+	}
+	os.Exit(0)
+}
+
+func ScreenshotTasks(url string, imageBuf *[]byte) chromedp.Tasks {
+	return chromedp.Tasks{
+		chromedp.Navigate(url),
+		chromedp.ActionFunc(func(ctx context.Context) (err error) {
+			*imageBuf, err = page.CaptureScreenshot().WithQuality(90).Do(ctx)
+			return err
+		}),
+	}
+}
